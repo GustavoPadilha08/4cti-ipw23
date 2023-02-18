05332e68e5826f7529d2b03ec2fc7e69244955f9
GustavoPadilha08 <108557193+GustavoPadilha08@users.noreply.github.com>
2023-02-17 22:21:25 -0300

    Add files via upload

---
+++ 4cti-JOAOeGUSTAVO/2_grid_system/1_grid_system_1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+
+  <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <header>
+
+          </header>
+      </div>
+
+
+      
+  </div>
+
+  

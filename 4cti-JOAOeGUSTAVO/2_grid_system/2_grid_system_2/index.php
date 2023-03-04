8f3852f816e1348b2ad0cc8df482ed83380b7a62
GustavoPadilha08 <108557193+GustavoPadilha08@users.noreply.github.com>
2023-03-03 22:11:33 -0300

    Add files via upload

---
+++ 4cti-JOAOeGUSTAVO/2_grid_system/2_grid_system_2/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+
+  <div class="container">
+   <div class="row">
+    <div class="col">auto</div>
+    <div class="col">auto</div>
+    <div class="col">auto</div>
+  </div>
+
+  <div class="row">
+    <div class="col">auto</div>
+    <div class="col-6">seis colunas</div>
+    <div class="col">auto</div>
+  </div>
+
+  <div class="row">
+    <div class="col">auto</div>
+    <div class="col-5">cinco colunas</div>
+    <div class="col">auto</div>
+  </div>
+
+  <div class="row">
+    <div class="col">auto</div>
+  </div>
+
+
+</body>
+</html>
---
+++ 4cti-JOAOeGUSTAVO/2_grid_system/3_grid_system_small/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-8">sm-8</div>
+      <div class="col-sm-4">sm-4</div>
+    </div>
+
+  <div class="row">
+    <div class="col">auto</div>
+    <div class="col-sm-5">sm-5</div>
+    <div class="col">auto</div>
+  </div>
+
+
+
+  Class md
+  <div class="row">
+    <div class="col-4 col-md-2"></div>
+    <div class="col-4 col-md-4"></div>
+    <div class="col-4 col-md-4"></div>
+
+  </div>
+  </div>
+
+
+</body>
+</html>
